988b0041b230e96514ba844e4cb967987682eaa5
Abhay <>
2021-05-26 16:00:43 +0000

    remove comment

--- src/test/java/me/replapiit/AppTest.java
+++ src/test/java/me/replapiit/AppTest.java
@@ -1,4 +1,3 @@
-/* I think we should remove junit because i do not know how to use it and I do not think it will be necessary, but we can keep it for now */
 package me.replapiit;
 
 import org.junit.Test;
