f8a42e6c05aab9042d6a47402124dc7d4db44bf1
Abhay <>
2021-05-26 16:01:39 +0000

    Changed spaces to tabs :P

--- src/test/java/me/replapiit/AppTest.java
+++ src/test/java/me/replapiit/AppTest.java
@@ -5,8 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class AppTest {
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(ReplAPI.test());
-    }
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(ReplAPI.test());
+	}
 }
